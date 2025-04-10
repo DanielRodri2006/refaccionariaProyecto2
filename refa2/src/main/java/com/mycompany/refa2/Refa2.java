3ada6ae109034d55d0f1bed6c876522a11e9ba47
rodri <rodri@Daniel>
2025-04-10 11:26:06 -0700

    cree el proyecto ahora si es el bueno

--- refa2/src/main/java/com/mycompany/refa2/Refa2.java
+++ refa2/src/main/java/com/mycompany/refa2/Refa2.java
@@ -11,6 +11,6 @@ package com.mycompany.refa2;
 public class Refa2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hoal universo!");
     }
 }
